6927472252fed61d17fd29bf25f08b3b107e764b
Vikash Anand <vikashanand338@gmail.com>
2022-07-12 22:58:32 +0530

    empty

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation/>}>
         <Route index element={<Home />}></Route>
+        
         <Route path="shop" element={<Shop />}></Route>
       </Route>
     </Routes>
